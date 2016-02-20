6f7cb3e2fdd1edbbad6a1c055a764f430beb5328
exquest <anthony.ainsworth@gmail.com>
2016-02-20 10:50:42 -0800

    created list model

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,28 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
+from lists.models import Item
 
+class ItemModelTest(TestCase):
+	
+	def test_saving_and_retrieving_items(self):
+		first_item = Item()
+		first_item_text = 'First list item'
+		first_item.text = 'First list item'
+		first_item.save()
+		
+		second_item = Item()
+		second_item_text = 'Second list item'
+		second_item.text = 'Second list item'
+		second_item.save()
+		
+		saved_items = Item.objects.all()
+		self.assertEqual(saved_items.count(), 2)
+		
+		first_saved_item = saved_items[0]
+		second_saved_item = saved_items[1]
+		self.assertEqual(first_saved_item.text, first_item_text)
+		self.assertEqual(second_saved_item.text, second_item_text)
 
 class HomePageTest(TestCase):
 	
