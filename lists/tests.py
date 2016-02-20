658061b607448b9fae7a357cc862ac7fc0e54b4b
exquest <anthony.ainsworth@gmail.com>
2016-02-20 11:29:41 -0800

    redirects after post

--- lists/tests.py
+++ lists/tests.py
@@ -29,6 +29,11 @@ class ItemModelTest(TestCase):
 
 class HomePageTest(TestCase):
 	
+	def test_home_page_only_saves_items_when_nessecary(self):
+		request = HttpRequest()
+		home_page(request)
+		self.assertEqual(Item.objects.count(), 0)
+	
 	def test_root_url_resolves_to_home_page(self):
 		found = resolve("/")
 		self.assertEqual(found.func, home_page)
@@ -46,9 +51,27 @@ class HomePageTest(TestCase):
 		
 		response = home_page(request)
 		
-		self.assertIn('A new list item', response.content.decode())
-		expected_html = render_to_string(
-			'lists/home.html',
-			{'new_item_text': 'A new list item'}
-		)
-		self.assertEqual(response.content.decode(), expected_html)
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+		
+	def test_home_page_reidrects_after_POST(self):
+		request = HttpRequest()
+		request.method = 'POST'
+		request.POST['item_text'] = 'A new list item'
+		
+		response = home_page(request)
+		
+		self.assertEqual(response.status_code, 302)
+		self.assertEqual(response['location'], '/')
+		
+	def test_home_page_displays_all_list_items(self):
+		Item.objects.create(text='1flf')
+		Item.objects.create(text='2flf')
+		
+		request = HttpRequest()
+		response = home_page(request)
+		
+		self.assertIn('1flf', response.content.decode())
+		self.assertIn('2flf', response.content.decode())
+		
