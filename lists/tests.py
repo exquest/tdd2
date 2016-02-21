1e12959bd8d040606783e78b62a9db213e38bba1
exquest <anthony.ainsworth@gmail.com>
2016-02-20 17:28:43 -0800

    implimented List model

--- lists/tests.py
+++ lists/tests.py
@@ -4,20 +4,28 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from lists.views import home_page
-from lists.models import Item
+from lists.models import Item, List
 
-class ItemModelTest(TestCase):
+class ListAndItemModelTest(TestCase):
 	
 	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+		
 		first_item = Item()
 		first_item_text = 'First list item'
 		first_item.text = 'First list item'
+		first_item.list = list_
 		first_item.save()
 		
 		second_item = Item()
 		second_item_text = 'Second list item'
 		second_item.text = 'Second list item'
+		second_item.list = list_
 		second_item.save()
+		
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
 		
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
@@ -25,7 +33,9 @@ class ItemModelTest(TestCase):
 		first_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, first_item_text)
+		self.assertEqual(first_saved_item.list, list_)
 		self.assertEqual(second_saved_item.text, second_item_text)
+		self.assertEqual(second_saved_item.list, list_)
 
 class HomePageTest(TestCase):
 	
@@ -43,8 +53,9 @@ class HomePageTest(TestCase):
 class ListViewTests(TestCase):
 	
 	def test_displays_all_items(self):
-		Item.objects.create(text='1flf')
-		Item.objects.create(text='2flf')
+		list_ = List.objects.create()
+		Item.objects.create(text='1flf', list=list_)
+		Item.objects.create(text='2flf', list=list_)
 		
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 		
