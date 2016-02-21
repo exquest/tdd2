2312bfee39f58beaf8268a0661b149ec7c7c7e1c
exquest <anthony.ainsworth@gmail.com>
2016-02-20 20:14:14 -0800

    towards the end of chap 6

--- lists/tests.py
+++ lists/tests.py
@@ -72,6 +72,14 @@ class ListViewTests(TestCase):
 		response = self.client.get('/lists/%d/' % (list_.id,))
 		self.assertTemplateUsed(response, 'lists/list.html')
 		
+	def test_passes_correct_list_to_template(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+		
+		response = self.client.get('/lists/%d/' % (correct_list.id,))
+		
+		self.assertEqual(response.context['list'], correct_list)
+		
 class NewListTests(TestCase):
 	
 	def test_saveing_a_POST_request(self):
@@ -84,10 +92,37 @@ class NewListTests(TestCase):
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')
 		
-	def test_reidrects_after_POST(self):
+	def test_redirects_after_POST(self):
 		response = self.client.post(
 			'/lists/new',
 			data={'item_text': 'A new list item'}
 		)
+		new_list = List.objects.first()
+		self.assertRedirects(response, '/lists/%d/' % (new_list.id,))
+
+class NewItemTests(TestCase):
+	
+	def test_can_save_POST_request_to_existing_list(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
 		
-		self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+		self.client.post(
+			'/lists/%d/add_item' % (correct_list.id),
+			data = {'item_text' : 'List item'},
+		)
+		
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'List item')
+		self.assertEqual(new_item.list, correct_list)
+		
+	def test_redirects_list_to_view(self):
+		other_list = List.objects.create()
+		correct_list = List.objects.create()
+		
+		response = self.client.post(
+			'/lists/%d/add_item' % (correct_list.id),
+			data = {'item_text' : 'List item'},
+		)
+		
+		self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
