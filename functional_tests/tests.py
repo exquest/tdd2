d5f01d74dec3074d7e29b251bcb21c82b4c73331
exquest <anthony.ainsworth@gmail.com>
2016-02-24 12:19:39 -0800

    first ft tests for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -102,3 +102,24 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
 
+	def test_layout_and_styling(self):
+		#Edith goes to the home page
+		self.browser.get(self.live_server_url)
+		self.browser.set_window_size(1024, 768)
+		
+		#The input window is nicely centered
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
+		
+		#She starts a new list and sees the input box is nicely centered there too
+		inputbox.send_keys('testing\n')
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertAlmostEqual(
+			inputbox.location['x'] + inputbox.size['width'] / 2,
+			512,
+			delta=5
+		)
